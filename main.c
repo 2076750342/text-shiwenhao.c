94b27ad163b79ad3e2b4fe0ae82be5ad559b3091
2076750342 <126095111+2076750342@users.noreply.github.com>
2023-03-24 20:42:45 +0800

    Add files via upload

--- main.c
+++ main.c
@@ -3,6 +3,13 @@
 
 int main()
 {
-printf("%d\n",sizeof(char));
+    int a,b,c,d;
+    scanf("%d",&a);
+    b=(a%6==0);
+    c=(a%2==0)||(a%3==0);
+    d=(a%2==0)&&(a%3!=0)||(a%2!=0)&&(a%3==0);
+    printf("%d divisible by 2 and 3?%d\n",a,b);
+    printf("%d divisible by 2 or 3?%d\n",a,c);
+    printf("%d divisible by 2 and 3,but not both?%d\n",a,d);
     return 0;
 }
